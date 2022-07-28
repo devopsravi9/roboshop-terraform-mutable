8e10a472bf13f90e6d1841211b9347c9aca07216
Ravi <ravi@gmail.com>
2022-07-28 13:15:10 +0530

    ec2 code

--- main.tf
+++ main.tf
@@ -4,3 +4,9 @@ module "vpc" {
   PROJECT = var.PROJECT
   VPC_CIDR = var.VPC_CIDR
 }
+
+module "subnet" {
+  source = "github.com/devopsravi9/tf-module-vpc"
+  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+}
