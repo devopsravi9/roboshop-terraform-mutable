a3cb5d63c346f579afd8e90dc59eb26153c94468
Ravi <ravi@gmail.com>
2022-08-06 14:08:49 +0530

    ec2 code

--- main.tf
+++ main.tf
@@ -1,28 +1,30 @@
 module "vpc" {
-  source = "github.com/devopsravi9/tf-module-vpc"
-  ENV = var.ENV
-  PROJECT = var.PROJECT
-  VPC_CIDR = var.VPC_CIDR
-  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+  source              = "github.com/devopsravi9/tf-module-vpc"
+  ENV                 = var.ENV
+  PROJECT             = var.PROJECT
+  VPC_CIDR            = var.VPC_CIDR
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  AZ = var.AZ
-  DEFAULT_VPC_ID =var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT =var.DEFAULT_VPC_RT
+  AZ                  = var.AZ
+  DEFAULT_VPC_ID      =var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT      =var.DEFAULT_VPC_RT
+  PUBLIC_ZONE_ID      = var.PUBLIC_ZONE_ID
+  PRIVATE_ZONE_ID     = var.PRIVATE_ZONE_ID
 }
 
 module "rds" {
-  source = "github.com/devopsravi9/tf-module-rds"
-  ENV = var.ENV
-  PROJECT = var.PROJECT
-  RDS_ENGINE = var.RDS_ENGINE
-  RDS_ENGINE_VERSION = var.RDS_ENGINE_VERSION
-  RDS_INSTANCE_CLASS = var.RDS_INSTANCE_CLASS
-  PRIVATE_SUBNET_ID = module.vpc.PRIVATE_SUBNET_ID
-  RGS_PORT = var.RGS_PORT
-  VPC_ID = module.vpc.VPC_ID
-  RDS_PG_FAMILY = var.RDS_PG_FAMILY
-  ALLOW_SG_CIDR = module.vpc.PRIVATE_SUBNET_CIDR
+  source              = "github.com/devopsravi9/tf-module-rds"
+  ENV                 = var.ENV
+  PROJECT             = var.PROJECT
+  RDS_ENGINE          = var.RDS_ENGINE
+  RDS_ENGINE_VERSION  = var.RDS_ENGINE_VERSION
+  RDS_INSTANCE_CLASS  = var.RDS_INSTANCE_CLASS
+  PRIVATE_SUBNET_ID   = module.vpc.PRIVATE_SUBNET_ID
+  RGS_PORT            = var.RGS_PORT
+  VPC_ID              = module.vpc.VPC_ID
+  RDS_PG_FAMILY       = var.RDS_PG_FAMILY
+  ALLOW_SG_CIDR       = module.vpc.PRIVATE_SUBNET_CIDR
 }
 
 module "docdb" {
--- vars.tf
+++ vars.tf
@@ -9,6 +9,8 @@ variable "AZ" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_CIDR" {}
 variable "DEFAULT_VPC_RT" {}
+variable "PRIVATE_ZONE_ID" {}
+variable "PUBLIC_ZONE_ID" {}
 
 //rds variables
 variable "RDS_ENGINE" {}
